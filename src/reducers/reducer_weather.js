0b6aa85700cdf23c5ef7e68d9dde4116ae336e0c
Ramiro <rama.dkno.k@gmail.com>
2017-11-29 09:45:24 -0800

    First commit

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER: 
+      // return state.concat([action.payload.data]) the same as below, the one below is es6
+      return [ action.payload.data, ...state ]
+  }
+  return state
+}
